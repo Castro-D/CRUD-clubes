2ecddac276c4a40f9b82e2acd2b0c83877beda13
Castro-D <castro.dh91@gmail.com>
2021-07-09 17:12:05 -0300

    agrega tabla basica con equipos

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const equipos = require('./data/equipos.json');
 
 const app = express();
 const PUERTO = 8080;
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+  layoutsDir: `${__dirname}/views/layouts`,
+  partialsDir: `${__dirname}/views/partials/`,
+}));
 
 app.get('/', (req, res) => {
-  res.send('hello world!');
+  res.render('main', { layout: 'index', equipos });
 });
 
 app.listen(PUERTO, () => {
