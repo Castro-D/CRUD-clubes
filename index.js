55be0c15b0f38a8dd90759168600398bf0f47050
Castro-D <castro.dh91@gmail.com>
2021-07-14 18:01:58 -0300

    agrega imagen con multer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const multer = require('multer');
 const equipos = require('./data/equipos.json');
 
+const upload = multer({ dest: './uploads' });
 const app = express();
 const PUERTO = 8080;
 
@@ -10,6 +12,8 @@ app.engine('handlebars', handlebars({
   layoutsDir: `${__dirname}/views/layouts`,
   partialsDir: `${__dirname}/views/partials/`,
 }));
+
+app.use(express.static(`${__dirname}/uploads`));
 
 app.get('/', (req, res) => {
   res.render('main', {
@@ -25,7 +29,22 @@ app.get('/equipo/:id/ver', (req, res) => {
   });
 });
 
+app.get('/equipo/crear', (req, res) => {
+  res.render('nuevo-equipo', {
+    layout: 'index',
+  });
+});
 
+app.post('/equipo/crear', upload.single('imagen'), (req, res) => {
+  console.log(req.file);
+  res.render('nuevo-equipo', {
+    layout: 'index',
+    data: {
+      mensaje: 'Exito!',
+      nombreArchivo: req.file.filename,
+    },
+  });
+});
 
 app.listen(PUERTO, () => {
   console.log(`Escuchando requests en https://localhost:${PUERTO}`);
